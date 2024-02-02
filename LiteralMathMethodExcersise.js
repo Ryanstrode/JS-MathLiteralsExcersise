f52ffc64df32899b7ae28db45b16ddfa68b3ae14
Ryan S <ryans@BCCSs-MacBook-Pro.local>
2024-02-02 10:41:09 -0600

    first commit

---
+++ LiteralMathMethodExcersise.js
@@ -0,0 +1,18 @@
+let warmHugs = "Hi, I'm Olaf, and I like warm hugs."
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("ik", "ov"))
+
+let beenImpaled = "Oh, Look at that. I've been Impaled."
+console.log(beenImpaled.slice(18, 36));
+
+let dotDotDot = '...'
+let skullBones = `I don\'t have a skull${dotDotDot}or bones.`
+console.log(skullBones);
+
+console.log(Math.PI);
+
+const randomNum = Math.random()*3
+randomNum = Math.floor(RandomNum) + 1
+console.log(randomNum);
+
+
